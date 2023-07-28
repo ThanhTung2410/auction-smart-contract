0af43533d9d9a205c3e84c83a8277279a3472724
Trần Phước Sang <52100303@student.tdtu.edu.vn>
2023-07-28 11:52:11 +0700

    feat: delete Roles(user.rs, impl_user)

--- contract/src/application/service/impl_user.rs
+++ contract/src/application/service/impl_user.rs
@@ -1,5 +1,5 @@
 use crate::models::contract::AuctionContractExt;
-use crate::models::user::{JsonUser, Roles, UserId, UserMetadata};
+use crate::models::user::{JsonUser, UserId, UserMetadata};
 use crate::models::{contract::AuctionContract, user::ImplUser};
 use near_sdk::{env, near_bindgen};
 
@@ -8,10 +8,10 @@ use near_sdk::{env, near_bindgen};
 impl ImplUser for AuctionContract {
     fn create_user(&mut self, name: String, avatar: Option<String>, email: String, phone: String, description: String,) {
         let owner_id = env::signer_account_id();
-        // assert!(
-        //     !self.user_metadata_by_id.contains_key(&owner_id),
-        //     "Account exists"
-        // );
+        assert!(
+            !self.user_metadata_by_id.contains_key(&owner_id),
+            "Account exists"
+        );
         let user = UserMetadata {
             user_id: owner_id.clone(),
             name,
@@ -19,7 +19,6 @@ impl ImplUser for AuctionContract {
             email,
             phone,
             description,
-            role: Roles::Participant,
         };
         let json_user = JsonUser {
             user_id: owner_id.clone(),
@@ -30,35 +29,6 @@ impl ImplUser for AuctionContract {
         };
         self.participant_users.insert(&owner_id);
         self.user_metadata_by_id.insert(&owner_id, &json_user);
-    }
-
-    fn update_role(&mut self, role: Roles) -> crate::models::user::JsonUser {
-        let owner_id = env::signer_account_id();
-        
-        let mut user_account = self.get_user_metadata_by_user_id(&owner_id).expect("Account does not exists");
-
-        self.update_user_information( 
-            user_account.metadata.name.clone(), 
-            user_account.metadata.avatar.clone(), 
-            user_account.metadata.email.clone(), 
-            user_account.metadata.phone.clone(), 
-            user_account.metadata.description.clone()
-        );
-
-        if role == Roles::Participant {
-            user_account.metadata.role = role;
-            self.participant_users.insert(&owner_id);
-            self.auctioneer_users.remove(&owner_id); // Remove from Auctioneer if present
-        } else if role == Roles::Auctioneer {
-            user_account.metadata.role = role;
-            self.auctioneer_users.insert(&owner_id);
-            self.participant_users.remove(&owner_id); // Remove from Participant if present
-        } else {
-            // Handle other roles if needed
-        }
-
-        self.user_metadata_by_id.insert(&owner_id, &user_account);
-        user_account
     }
 
     fn get_user_metadata_by_user_id(&self, user_id: &UserId) -> Option<JsonUser> {
@@ -86,10 +56,5 @@ impl ImplUser for AuctionContract {
         limit: Option<u32>,
     ) -> Vec<crate::models::user::JsonUser> {
         todo!()
-    }
-
-    fn check_user_role(&self , user_id: crate::models::user::UserId) -> crate::models::user::Roles {
-        let user_account = self.get_user_metadata_by_user_id(&user_id).expect("Account does not exists");
-        user_account.metadata.role
     }
 }
